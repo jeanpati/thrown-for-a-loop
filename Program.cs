508944e3da09674232c496da66df2d735d294055
Jean Patimeteeporn <jpatimet@gmail.com>
2024-06-06 17:28:38 -0500

    added greeting and asked for a response in the console

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+string greeting = @"Welcome to Thrown For a Loop
+Your one-stop shop for used sporting equipment";
+Console.WriteLine(greeting);
+Console.WriteLine("Please enter a product name: ");
+
+string response = Console.ReadLine();
+Console.WriteLine(@$"You chose: {response}. 
+Thank you for your input!");
